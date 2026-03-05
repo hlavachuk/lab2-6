e39d280d8206385b7c7bb9c3293b7e569eda5024
hlavachuk <bogdan1231qq@gmail.com>
2026-03-05 19:01:51 +0200

    Added Rectangle.h header file

---
+++ lab2/rectangle.h
@@ -0,0 +1,21 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle();
+    Rectangle(double width, double height = 1);
+
+    ~Rectangle();
+
+    double area() const;
+    double perimeter() const;
+
+    void display() const;
+};
+
+#endif
