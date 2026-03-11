3e21d1af0175832833aa1b771a0ab0f07dd19487
hlavachuk <bogdan1231qq@gmail.com>
2026-03-11 18:00:25 +0200

    Added copy/move constructors, static field, operator overloading

--- lab2/triangle.h
+++ lab2/triangle.h
@@ -1,6 +1,8 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include <iostream>
+
 using namespace std;
 
 class Triangle {
@@ -9,16 +11,29 @@ private:
     double b;
     double c;
 
+    static int triangleCount;   
+
 public:
     Triangle();
-    Triangle(double a, double b, double c = 1);
+    Triangle(double a, double b, double c);
+
+    Triangle(const Triangle& other);
+    Triangle(Triangle&& other);
 
     ~Triangle();
 
-    double area() const;
     double perimeter() const;
+    double area() const;
 
     void display() const;
+    void setA(double a);
+
+    static int getCount();
+
+    Triangle operator+(const Triangle& other);
+
+    friend ostream& operator<<(ostream& os, const Triangle& t);
+    friend istream& operator>>(istream& is, Triangle& t);
 };
 
 #endif
