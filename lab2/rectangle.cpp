220d7116da07859f0578d20b9fdca283037fbb5d
hlavachuk <bogdan1231qq@gmail.com>
2026-03-30 04:44:08 +0300

    updated rectangle.h and rectangle.cpp with inheritance and HAS-A

--- lab2/rectangle.cpp
+++ lab2/rectangle.cpp
@@ -1,16 +1,36 @@
-#include "Rectangle.h"
-#include <iostream>
-
+#include "rectangle.h"
 using namespace std;
 
-Rectangle::Rectangle() : Rectangle(1, 1) {}
+Rectangle::Rectangle()
+    : Polygon("Rectangle", 4), width(1), height(1), topLeft(0, 0) {
+}
 
-Rectangle::Rectangle(double width, double height)
-    : width(width), height(height) {
+Rectangle::Rectangle(double width, double height, Point topLeft)
+    : Polygon("Rectangle", 4), width(width), height(height), topLeft(topLeft) {
+}
+
+Rectangle::Rectangle(const Rectangle& other)
+    : Polygon(other), width(other.width), height(other.height), topLeft(other.topLeft) {
+}
+
+Rectangle::Rectangle(Rectangle&& other) noexcept
+    : Polygon(move(other)), width(other.width), height(other.height), topLeft(move(other.topLeft)) {
+    other.width = 0;
+    other.height = 0;
+}
+
+Rectangle& Rectangle::operator=(const Rectangle& other) {
+    if (this != &other) {
+        Polygon::operator=(other);
+        width = other.width;
+        height = other.height;
+        topLeft = other.topLeft;
+    }
+    return *this;
 }
 
 Rectangle::~Rectangle() {
-    cout << "Rectangle destroyed\n";
+    cout << "Rectangle destroyed" << endl;
 }
 
 double Rectangle::area() const {
@@ -22,9 +42,15 @@ double Rectangle::perimeter() const {
 }
 
 void Rectangle::display() const {
-    cout << "Rectangle width: " << width
-        << ", height: " << height
-        << ", Area: " << area()
-        << ", Perimeter: " << perimeter()
-        << endl;
+    Polygon::display();
+    cout << "rectangle: " << width << " x " << height << endl;
+    cout << "top left: ";
+    topLeft.display();
+    cout << endl;
+    cout << "area: " << area() << endl;
+    cout << "perimeter: " << perimeter() << endl;
+}
+
+int Rectangle::getCount() {
+    return Shape::getCount();
 }
--- lab2/rectangle.h
+++ lab2/rectangle.h
@@ -1,23 +1,30 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include "triangle.h"
 using namespace std;
 
-class Rectangle {
+class Rectangle : public Polygon {
 private:
     double width;
     double height;
+    Point topLeft;   // HAS-A
 
 public:
     Rectangle();
-    Rectangle(double width, double height = 1);
+    Rectangle(double width, double height, Point topLeft = Point());
+
+    Rectangle(const Rectangle& other);
+    Rectangle(Rectangle&& other) noexcept;
+    Rectangle& operator=(const Rectangle& other);
 
     ~Rectangle();
 
-    double area() const;
-    double perimeter() const;
+    double area() const override;
+    double perimeter() const override;
+    void display() const override;
 
-    void display() const;
+    static int getCount();
 };
 
 #endif
