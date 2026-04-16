f14fd8771159c7cb2aa045390520e0734946f304
hlavachuk <bogdan1231qq@gmail.com>
2026-04-16 10:12:09 +0300

    Update Shape class

--- lab2/Shape.h
+++ lab2/Shape.h
@@ -5,12 +5,13 @@
 #include <string>
 using namespace std;
 
-class Drawable { // Interface  клас тільки з pure virtual функціями
+class Drawable {
 public:
-    virtual void draw() const = 0; //Полік
-    virtual ~Drawable() {}  
+    virtual void draw() const = 0;
+    virtual ~Drawable() {}
+};
 
-class Shape : public Drawable { // Interface  клас тільки з pure virtual функціями
+class Shape : public Drawable {
 protected:
     string name;
     static int count;
@@ -20,15 +21,15 @@ public:
     Shape(const Shape& other);
     Shape(Shape&& other) noexcept;
     Shape& operator=(const Shape& other);
-    virtual ~Shape(); // Virtual destructor потрібен для правильного видалення через вказівник на батьківський клас (Любий)
+    virtual ~Shape();
 
-    virtual double area() const = 0;     // Pure virtual function  метод без реалізації
+    virtual double area() const = 0;
     virtual double perimeter() const = 0;
     virtual void display() const;
     virtual void info() const;
-    virtual void draw() const override = 0;
+    virtual void draw() const = 0;
 
-    void showType() const; //Static binding (МЕТОД!!!!!)
+    void showType() const;
 
     static int getCount();
 };
